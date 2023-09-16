d8f6ea5463004d93c0055ef223563a0d33f2adf3
John Arundel <john@bitfieldconsulting.com>
2023-09-16 17:33:31 +0100

    add adaptive rate limiting, stats

--- weaver.go
+++ weaver.go
@@ -7,51 +7,76 @@ import (
 	"log"
 	"net/http"
 	"net/url"
+	"os"
+	"os/signal"
 	"strings"
+	"time"
 
 	"github.com/antchfx/htmlquery"
 	"golang.org/x/time/rate"
 )
 
-var visited = map[string]bool{}
-
-var limiter = rate.NewLimiter(5, 1)
+const maxRate = 5
 
 var (
-	base    *url.URL
-	verbose *bool
-)
-
-func Main() int {
-	verbose = flag.Bool("v", false, "verbose output")
-	flag.Parse()
-	tmp, err := url.Parse(flag.Args()[0])
-	base = tmp
-	if err != nil {
-		log.Fatal(err)
+	start           = time.Now()
+	visited         = map[string]bool{}
+	warning, broken []string
+	success         int
+	limiter         = rate.NewLimiter(maxRate, 1)
+	lastRateLimit   time.Time
+	base            *url.URL
+	verbose         *bool
+	httpClient      = &http.Client{
+		Timeout: 5 * time.Second,
 	}
-	ctx := context.Background()
-	Crawl(ctx, base.String(), "none")
-	return 0
-}
+)
 
 func Crawl(ctx context.Context, page, referrer string) {
 	limiter.Wait(ctx)
-	resp, err := http.Get(page)
+	resp, err := httpClient.Get(page)
 	if err != nil {
 		fmt.Printf("[%s] %s (referrer %s)\n", err, page, referrer)
+		broken = append(broken, fmt.Sprintf("[%s] %s (referrer %s)\n", err, page, referrer))
 		return
+	}
+	if resp.StatusCode == http.StatusTooManyRequests {
+		limiter.SetLimit(limiter.Limit() / 2)
+		if *verbose {
+			fmt.Printf("[%s] %s, reducing rate limit to %.2fr/s\n", resp.Status, page, limiter.Limit())
+		}
+		lastRateLimit = time.Now()
+		Crawl(ctx, page, referrer)
+		return
+	}
+	curLimit := limiter.Limit()
+	if curLimit < maxRate && time.Since(lastRateLimit) > 10*time.Second {
+		curLimit *= 1.5
+		if curLimit > maxRate {
+			curLimit = maxRate
+		}
+		limiter.SetLimit(curLimit)
+		lastRateLimit = time.Now()
+		if *verbose {
+			fmt.Printf("increasing rate limit to %.2fr/s\n", curLimit)
+		}
 	}
 	if resp.StatusCode != http.StatusOK {
 		fmt.Printf("[%s] %s (referrer: %s)\n", resp.Status, page, referrer)
+		if resp.StatusCode == http.StatusNotFound {
+			broken = append(broken, fmt.Sprintf("[%s] %s (referrer: %s)\n", resp.Status, page, referrer))
+		} else {
+			warning = append(warning, fmt.Sprintf("[%s] %s (referrer: %s)\n", resp.Status, page, referrer))
+		}
 		return
 	}
+	success++
 	if *verbose {
 		fmt.Printf("[%s] %s (referrer: %s)\n", resp.Status, page, referrer)
 	}
 	defer resp.Body.Close()
-	if isOffsite(page) {
-		return
+	if !strings.HasPrefix(page, base.String()) {
+		return // skip parsing offsite pages
 	}
 	doc, err := htmlquery.Parse(resp.Body)
 	if err != nil {
@@ -63,25 +88,40 @@ func Crawl(ctx context.Context, page, referrer string) {
 		u, err := url.Parse(link)
 		if err != nil {
 			fmt.Printf("[%s] %s (referrer: %s)\n", err, link, referrer)
+			broken = append(broken, fmt.Sprintf("[%s] %s (referrer: %s)\n", err, link, referrer))
 			return
 		}
 		link = base.ResolveReference(u).String()
 		if !visited[link] {
 			visited[link] = true
 			Crawl(ctx, link, page)
-		} else if *verbose {
-			fmt.Printf("[skip] %s (referrer: %s)\n", link, referrer)
 		}
 	}
 }
 
-func isOffsite(link string) bool {
-	return !strings.HasPrefix(link, base.String())
-}
-
-func canonicalize(link string) string {
-	if !strings.HasPrefix(link, "http") {
-		return base.String() + "/" + link
+func Main() int {
+	verbose = flag.Bool("v", false, "verbose output")
+	flag.Parse()
+	tmp, err := url.Parse(flag.Args()[0])
+	base = tmp
+	if err != nil {
+		log.Fatal(err)
 	}
-	return link
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+	go Crawl(ctx, base.String(), "CLI")
+	<-ctx.Done()
+	if len(broken) > 0 {
+		fmt.Println("\nBroken links:")
+		fmt.Println(strings.Join(broken, ""))
+	}
+	fmt.Printf("\nTime: %s Visited: %d Links: %d OK: %d Broken: %d Warning: %d\n",
+		time.Since(start).Round(time.Second),
+		len(visited),
+		success+len(broken)+len(warning),
+		success,
+		len(broken),
+		len(warning),
+	)
+	return 0
 }
